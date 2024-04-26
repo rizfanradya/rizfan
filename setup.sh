d52e23023388c57916927f959fbe8f933b158b15
rizfanradya <rizfankusuma@gmail.com>
2024-04-26 09:49:39 +0000

    up

--- setup.sh
+++ setup.sh
@@ -1,11 +1,25 @@
 sudo apt get update
 sudo apt get upgrade
-sudo apt-get install gnupg curl
+sudo apt-get install curl
+sudo apt-get install gnupg
+sudo apt-get install ca-certificates
+
+# mongodb
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+# pgadmin
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+
+# nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# docker
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt get upgrade
@@ -15,10 +29,17 @@ sudo apt-get install -y mysql-server
 sudo apt-get install -y mongodb-org
 sudo apt-get install -y postgresql-common
 sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+sudo apt-get install phpmyadmin
 sudo apt-get install pgadmin4-web
 # sudo /usr/pgadmin4/bin/setup-web.sh
-sudo apt-get install phpmyadmin
 
 sudo apt-get install python3
 sudo apt-get install npm
 nvm install --lts
+
+sudo apt-get install docker-ce
+sudo apt-get install docker-ce-cli
+sudo apt-get install containerd.io
+sudo apt-get install docker-buildx-plugin
+sudo apt-get install docker-compose
+sudo apt-get install docker-compose-plugin
