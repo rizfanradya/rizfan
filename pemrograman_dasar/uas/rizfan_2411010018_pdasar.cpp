7dc668ddeb3a70182bdaf858ce1358a96955330f
rizfanradya <rizfankusuma@gmail.com>
2025-01-13 10:31:18 +0700

    up

--- pemrograman_dasar/uas/rizfan_2411010018_pdasar.cpp
+++ pemrograman_dasar/uas/rizfan_2411010018_pdasar.cpp
@@ -43,11 +43,6 @@ int main()
             cin >> num2;
             cout << endl;
 
-            cout << "Sebelum ditukar: " << endl;
-            cout << "Angka pertama: " << num1 << endl;
-            cout << "Angka kedua: " << num2 << endl;
-            cout << endl;
-
             temp = num1;
             num1 = num2;
             num2 = temp;
